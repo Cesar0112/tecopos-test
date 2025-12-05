778a41034a6afcc313add5a76b51e54bfae166be
Cesar0112 <cesarfernandezgarcia349@gmail.com>
2025-12-05 12:26:22 -0500

    fix: Arreglado que no cargaba el secret de JWT

--- sso/src/auth/auth.module.ts
+++ sso/src/auth/auth.module.ts
@@ -6,12 +6,13 @@ import { JwtModule } from '@nestjs/jwt';
 import { User } from '../users/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtStrategy } from './jwt.strategy';
-import { PassportModule } from '@nestjs/passport';
+
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
         TypeOrmModule.forFeature([User]),
-        PassportModule.register({ defaultStrategy: 'jwt' }),
         JwtModule.register({
             secret: process.env.JWT_SECRET,
             signOptions: { expiresIn: '24h' },
