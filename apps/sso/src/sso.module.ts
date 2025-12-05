29c2bb890b74c8145fe0a1d873413718e987e4af
Cesar0112 <cesarfernandezgarcia349@gmail.com>
2025-12-04 19:40:08 -0500

    fix: arreglada la configuración de typeorm, con postgresql y la configuración

--- apps/sso/src/sso.module.ts
+++ apps/sso/src/sso.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { SsoController } from './sso.controller';
-import { SsoService } from './sso.service';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [SsoController],
-  providers: [SsoService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+    TypeOrmModule.forFeature([User]),
+    AuthModule,],
+  providers: [],
 })
-export class SsoModule {}
+export class SsoModule { }
