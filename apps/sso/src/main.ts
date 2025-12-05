1ab2f1acebe262e85307eb072f42cc4a7c27c7dc
Cesar0112 <cesarfernandezgarcia349@gmail.com>
2025-12-04 19:39:12 -0500

    docs: agregada el inicio de la configuración de swagger de microservicio sso

--- apps/sso/src/main.ts
+++ apps/sso/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { SsoModule } from './sso.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(SsoModule);
-  await app.listen(process.env.port ?? 3000);
+
+
+  const config = new DocumentBuilder()
+    .setTitle('SSO')
+    .setDescription('Prueba técnica backend')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // → http://localhost:3000/api
+
+  await app.listen(3000);
+  console.log(`SSO is running on: http://localhost:3000`);
 }
 bootstrap();
