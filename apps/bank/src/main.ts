5d35c2927acf95b8f2363e131a0055117697207d
Cesar0112 <cesarfernandezgarcia349@gmail.com>
2025-12-04 19:54:00 -0500

    docs: Agregada la documentación de swagger de bank

--- apps/bank/src/main.ts
+++ apps/bank/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { BankModule } from './bank.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(BankModule);
-  await app.listen(process.env.port ?? 3000);
+  const serviceName = 'Bank';
+  const config = new DocumentBuilder()
+    .setTitle(`${serviceName} - TECOPOS`)
+    .setDescription('Prueba técnica backend')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // → http://localhost:3000/api
+  await app.listen(3000);
 }
 bootstrap();
