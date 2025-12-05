f9f74f4d27c1b910aae230f12cd326cd37e4f894
Cesar0112 <cesarfernandezgarcia349@gmail.com>
2025-12-05 13:58:53 -0500

    docs: Documenté los endpoints de login y register

--- sso/src/dto.ts
+++ sso/src/dto.ts
@@ -1,9 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, MinLength } from "class-validator";
+
 export class RegisterDto {
+    @ApiProperty({ example: 'user@example.com', description: 'Correo electrónico' })
+    @IsEmail()
     email: string;
+
+    @ApiProperty({ example: 'StrongP@ssw0rd', description: 'Contraseña' })
+    @IsString()
+    @MinLength(8)
     password: string;
 }
 
-export class LoginDto {
-    email: string;
-    password: string;
+export class LoginDto extends RegisterDto {
+
 }
--- sso/src/sso.controller.ts
+++ sso/src/sso.controller.ts
@@ -2,24 +2,26 @@ import { Controller, Post, Body, UseGuards, Get, Request } from '@nestjs/common'
 import { RegisterDto, LoginDto } from './dto';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { SsoService } from './sso.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class SsoController {
   constructor(private readonly ssoService: SsoService) { }
 
   @Post('register')
+  @ApiOperation({ summary: 'Registro de usuario' })
+  @ApiResponse({ status: 201, description: 'Usuario creado exitosamente' })
+  @ApiResponse({ status: 400, description: 'Datos inválidos' })
   register(@Body() dto: RegisterDto) {
     return this.ssoService.register(dto);
   }
 
   @Post('login')
+  @ApiOperation({ summary: 'Login de usuario' })
+  @ApiResponse({ status: 200, description: 'Login exitoso, devuelve JWT' })
+  @ApiResponse({ status: 401, description: 'Credenciales inválidas' })
   login(@Body() dto: LoginDto) {
     return this.ssoService.login(dto);
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('me')
-  me(@Request() req) {
-    return req.user;
-  }
 }
